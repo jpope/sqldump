95715324490e8102a56e4e1a4d6c8df9f1318adf
Justin Pope <github@jpo.pe>
2019-03-11 12:47:26 -0500

    Commit proposed changes from Aldo for his usage/configurability in ERA Project

---
+++ Configuration/DumpRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SQLDump.Configuration
+{
+    internal class DumpRequest
+    {
+        public string ConnectionString { get; set; }
+
+        public bool ListIsExclusive { get; set; }
+
+        public string OutputDirectory { get; set; }
+
+        public string FileNamePrefix { get; set; }
+
+        public string FileNameSuffix { get; set; }
+
+        public List<TableRequest> TableRequests { get; set; }
+    }
+}
---
+++ Configuration/TableRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SQLDump.Configuration
+{
+    internal class TableRequest
+    {
+        public string Name { get; set; }
+
+        public string IdentityColumn { get; set; }
+
+        public int? Limit { get; set; }
+
+        public bool IncludeIdentityInsert { get; set; }
+    }
+}
