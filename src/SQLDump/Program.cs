36dd2ec7e15b34dbf13ba0b773192793956c6840
Justin Pope <github@jpo.pe>
2019-03-12 18:57:03 -0500

    Move logic for "GetInsertStatements into it's own class; keep as static for now, to minimize signature changes

--- src/SQLDump/Program.cs
+++ src/SQLDump/Program.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using NDesk.Options;
+using SQLDump.SqlGeneration;
 
 namespace SQLDump
 {
@@ -264,46 +265,7 @@ order by
 
             private static string GetInsertStatement(TableInfo table, IDataRecord reader, bool includeIdentityInsert)
             {
-                StringBuilder builder = new StringBuilder("");
-                builder.Append("insert into [" + table.Name + "] (");
-                bool flag = true;
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    string name = reader.GetName(i);
-                    if (includeIdentityInsert || (name != table.IdentityColumn))
-                    {
-                        if (flag)
-                        {
-                            flag = false;
-                        }
-                        else
-                        {
-                            builder.Append(", ");
-                        }
-                        builder.Append("[" + name + "]");
-                    }
-                }
-                builder.Append(") values (");
-                flag = true;
-                for (int j = 0; j < reader.FieldCount; j++)
-                {
-                    string str2 = reader.GetName(j);
-                    if (includeIdentityInsert || (str2 != table.IdentityColumn))
-                    {
-                        if (flag)
-                        {
-                            flag = false;
-                        }
-                        else
-                        {
-                            builder.Append(", ");
-                        }
-                        string str3 = ConvertToSqlLiteral(reader.GetFieldType(j), reader.GetValue(j));
-                        builder.Append(str3);
-                    }
-                }
-                builder.Append(")");
-                return builder.ToString();
+                return SqlGenerator.GetInsertStatement(table, reader, includeIdentityInsert);
             }
 
         //        private static void DumpTable(IDbConnection connection, TableInfo table, bool includeIdentityInsert, int? limit)
@@ -346,56 +308,6 @@ order by
         //				Console.WriteLine("set identity_insert [" + table + "] off");
         //			}
         //		}
-
-		private static string ConvertToSqlLiteral(Type type, object value)
-		{
-			if (value == DBNull.Value)
-			{
-				return "null";
-			}
-			else if (type == typeof (string))
-			{
-				return "'" + ((string) value).Replace("'", "''") + "'";
-			}
-			else if (type == typeof (DateTime))
-			{
-				return "'" + ((DateTime) value).ToString("yyyy-MM-dd hh:mm:ss.fff") + "'";
-			}
-			else if (type == typeof (DateTimeOffset))
-			{
-				return "'" + ((DateTimeOffset) value).ToString("yyyy-MM-dd hh:mm:ss.fff") + "'";
-			}
-			else if (type == typeof (byte[]))
-			{
-				return GetHexString((byte[]) value);
-			}
-			else if (type == typeof (Guid))
-			{
-				return "'" + ((Guid) value).ToString("D") + "'";
-			}
-			else if (type == typeof (bool))
-			{
-				return ((bool) value) ? "1" : "0";
-			}
-			else
-			{
-				return value.ToString();
-			}
-		}
-
-		private static string GetHexString(IEnumerable<byte> value)
-		{
-			var sb = new StringBuilder("'0x");
-
-			foreach (var @byte in value)
-			{
-				sb.AppendFormat("{0:x2}", @byte);
-			}
-
-			sb.Append("'");
-
-			return sb.ToString();
-		}
 
 		private static void PrintError(string message)
 		{
