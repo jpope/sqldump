281061b4961ec4911b23f6ff62ff065ca9382396
Justin Pope <github@jpo.pe>
2019-03-18 15:59:37 -0500

    Move class for generating a connection string into it's own, unused class

--- src/SQLDump/Program.cs
+++ src/SQLDump/Program.cs
@@ -139,18 +139,6 @@ namespace SQLDump
 			}
 		}
 
-		private static string GetConnectionString(string server, string database, bool useSqlServerAuthenication, string username, string password)
-		{
-			if (useSqlServerAuthenication)
-			{
-				return string.Format("Data Source={0};Initial Catalog={1};User Id={2};Password={3};", server, database, username, password);
-			}
-			else
-			{
-				return string.Format("Data Source={0};Initial Catalog={1};Integrated Security=SSPI;Trusted_Connection=yes;", server, database);
-			}
-		}
-
 		private static IEnumerable<TableInfo> GetTablesToDump(IDbConnection connection, ICollection<string> tableNames, bool listIsExclusive)
 		{
 			const string sqlFormat =
