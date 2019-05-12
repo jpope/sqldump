bcc9a56225383145a8f41d7d75b1df8aa6b87221
Justin Pope <github@jpo.pe>
2019-05-11 20:29:23 -0500

    re-order columns to similar order

--- src/SQLDump/Configuration/DumpRequest.cs
+++ src/SQLDump/Configuration/DumpRequest.cs
@@ -5,15 +5,10 @@ namespace SQLDump.Configuration
     public class DumpRequest
     {
         public string ConnectionString { get; set; }
-
-        public bool ListIsExclusive { get; set; }
-
         public string OutputDirectory { get; set; }
-
-        public string FileNamePrefix { get; set; }
-
-        public string FileNameSuffix { get; set; }
-
+        public bool ListIsExclusive { get; set; }
         public List<TableInfo> TableRequests { get; set; }
+        public string FileNamePrefix { get; set; }
+        public string FileNameSuffix { get; set; }
     }
 }
