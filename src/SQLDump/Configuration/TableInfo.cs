1f959f3a2278f6ee537d2c5d8d6402480e101e5a
Justin Pope <github@jpo.pe>
2019-05-11 20:26:31 -0500

    Refactor config class changes made in master

--- src/SQLDump/Configuration/TableInfo.cs
+++ src/SQLDump/Configuration/TableInfo.cs
@@ -3,7 +3,12 @@
     public class TableInfo
     {
         public string Name { get; set; }
+        public string Schema { get; set; }
         public string IdentityColumn { get; set; }
+        public string SchemaAndTableName
+        {
+            get { return $"[{Schema}].[{Name}]"; }
+        }
         public int? Limit { get; set; }
         public bool IncludeIdentityInsert { get; set; }
     }
